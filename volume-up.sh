dd8a8c8d3f8638ae488e10bd7fbc06a01fce32b3
Sohail Haqyar <sohail.haqyar@salientsys.com>
2024-06-18 12:06:58 -0500

    Stuff

---
+++ volume-up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get the current volume
+current_volume=$(playerctl volume)
+
+# Increase the volume by 0.1
+new_volume=$(echo "$current_volume + 0.1" | bc)
+
+# Set the new volume
+playerctl volume "$new_volume"
+
+echo "Volume increased to $new_volume"
