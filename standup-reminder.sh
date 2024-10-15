c7b4adfb1304f3014964c0470402d53d5522a2d8
Sohail Haqyar <haqyarsohail@gmail.com>
2024-10-15 16:17:06 -0500

    Added new scripts

---
+++ standup-reminder.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while true; do
+  current_hour=$(date +"%H")
+  current_brightness=$(brightnessctl get)
+
+  if [ "$current_hour" -ge 20 ]; then
+    if [ "$current_brightness" -gt 20 ]; then
+      notify-send "Lowering brightness.."
+      brightnessctl set 20
+    fi
+  else
+    if [ "$current_brightness" -lt 255 ]; then
+      notify-send "Let there be light..."
+      brightnessctl set 255
+    fi
+  fi
+  sleep 1800
+done
